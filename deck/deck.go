d11248e57afc5d9e07d2035646bdf780d9fd8e2a
Juan Xavier Valverde <juanxaviervm@hotmail.com>
2023-10-03 16:14:57 -0500

    deck created

--- deck/deck.go
+++ deck/deck.go
@@ -52,6 +52,27 @@ func NewCard(s Suit, v int) Card {
 	}
 }
 
+
+type Deck [52] Card
+
+func New() Deck {
+	var (
+		nSuits = 4
+		nCards =13
+		d = [52] Card {}
+	)
+	x := 0
+
+	for i := 0; i < nSuits; i++ {
+		for j := 0; j < nCards; j++ {
+			d[x] = NewCard(Suit(i), j+1)
+			x++
+		}
+	}
+	
+	return d
+}
+
 func suitToUnicode(s Suit) string {
 	switch s  {
 		case Spades:
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 ) 
 
 func main () {
-	card := deck.NewCard(deck.Spades, 3)
-	fmt.Println(card)
+	// card := deck.NewCard(deck.Spades, 3)
+	d := deck.New()
+	fmt.Println(d)
 }
