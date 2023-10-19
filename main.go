75d9d3633f710d69248ac23f446e3e3901c61dbe
Juan Xavier Valverde <juanxaviervm@hotmail.com>
2023-10-19 16:45:54 -0500

    all handshakes working

--- main.go
+++ main.go
@@ -12,12 +12,18 @@ func main() {
 	playerB := makeServerAndStart(":4000")
 	playerC := makeServerAndStart(":5000")
 	playerD := makeServerAndStart(":6000")
+	playerE := makeServerAndStart(":7000")
+	playerF := makeServerAndStart(":8000")
 
-	time.Sleep(1 * time.Second)
 	playerB.Connect(playerA.ListenAddr)
 	time.Sleep(1 * time.Second)
 	playerC.Connect(playerB.ListenAddr)
+	time.Sleep(1 * time.Second)
 	playerD.Connect(playerC.ListenAddr)
+	time.Sleep(1 * time.Second)
+	playerE.Connect(playerD.ListenAddr)
+	time.Sleep(1 * time.Second)
+	playerF.Connect(playerE.ListenAddr)
 
 	select {}
 }
@@ -32,4 +38,7 @@ func makeServerAndStart(addr string) *p2p.Server {
 	go server.Start()
 	time.Sleep(1 * time.Second)
 	return server
+}
+
+func testPeersConnected(s *p2p.Server) {
 }
